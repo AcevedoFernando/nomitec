d4bcfb416971636f333e3923de320719ad6b55bd
Padrinii <luis.regalado123456@gmail.com>
2019-11-27 16:57:17 -0600

    backend ya con bajas

--- backend/empleados/bajas.php
+++ backend/empleados/bajas.php
@@ -1,39 +1,24 @@
-<?php
-//include ('../conexionmysql.php');
 
-
-//$id=$_GET['id'];
-//$conn=new mysqli($server,$username,$password,$db);
-//$borrar=" DELETE FROM empleados WHERE id='$id'";
-//if ($conn->query($sql) == TRUE) {
-//	echo "si se borro mi rega";
-//}else{
-//	"al chile no borro".$conn->error;
-//}
-//$conn->close();
-//header('location: /nomitec/views/empleados/empleados.php');
-//?>
 <?php
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "nomitec";
+$id=$_GET['id'];
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+// Creamos la conexion con la bd
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// checamos conexion
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
-
-// sql to delete a record
-$sql = "DELETE FROM MyGuests WHERE id=3";
-
-if ($conn->query($sql) === TRUE) {
+// instruccion para borrar el empleado que necesitamos borrar
+$sql = "DELETE FROM empleado WHERE id='$id'";
+//comprobacion de que se halla borrado el empleado
+if (mysqli_query($conn, $sql)) {
     echo "Record deleted successfully";
 } else {
-    echo "Error deleting record: " . $conn->error;
+    echo "Error deleting record: " . mysqli_error($conn);
 }
-
-$conn->close();
+mysqli_close($conn);
 ?>
