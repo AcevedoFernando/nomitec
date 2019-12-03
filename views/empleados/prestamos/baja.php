c45b9f1bc9a195d1d2f7f6490f41dbffafea1d18
Fernando Acevedo <fernando.acevedo@cceo.com.mx>
2019-12-02 19:49:28 -0600

    Se agregaron las vistas de prestamos

---
+++ views/empleados/prestamos/baja.php
@@ -0,0 +1,63 @@
+<?php
+    require ('../../../backend/sesiones/validarsesion.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../../css/home.css">
+    <link href="../../../open-iconic/font/css/open-iconic-bootstrap.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <title>Nomitec | Registrar Empleado</title>
+    
+    <style>
+        body {
+            background-image: url('/nomitec/resources/home.jpg');
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-size: cover;
+        }
+    </style>
+
+</head>
+<body>
+    <?php
+        include ('../../../layouts/menu.php');
+
+        $id = $_GET['id'];             
+    ?>
+<div class="jumbotron mx-5">       
+    <div class="row justify-content-start">
+        <form action="prestamos.php">
+            <button class="ml-5 btn btn-info" type="submit">Regresar</button>                
+        </form>
+    </div>
+            <br>
+    <div class="row justify-content-center border border-primary rounded bg-primary">            
+                <h3>Dar de baja</h3>             
+    </div>
+    <div class="row justify-content-center border border-primary rounded">                    
+                <div class="col-12 form-group text-center">
+                    <br>
+                    <h2>Esta seguro en de dar de baja el prestamo: <br> <?php echo $id; ?></h2>   
+                </div>
+                <div class="input-group justify-content-center">
+                    <button type="button" class="input-item btn btn-danger" onclick="location.href='/nomitec/backend/empleados/prestamos/bajas.php?id=<?php echo $id ?>'">Si, estoy de acuerdo </button>   
+                    <button type="button" class="input-item btn btn-warning" onclick="location.href='/nomitec/views/empleados/prestamos/prestamos.php'">No, regresar al listado </button>   
+                </div> 
+                <br>
+    </div>               
+</div>
+
+
+    <?php
+        include ('../../../layouts/footer.php');
+    ?>
+</body>
+</html>
